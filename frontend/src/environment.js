aff28b5cf735ce3de3668e05d083a39e302f7f53
AyushGhole <ayushghole@gmail.com>
2025-03-06 17:43:31 +0530

    Update  Environment Varaibles

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,6 @@
 let IS_PROD = true;
-const server = "http://localhost:5000";
+const server = IS_PROD
+  ? " https://meetvideocallbackend.onrender.com "
+  : "http://localhost:5000";
 
 export default server;
